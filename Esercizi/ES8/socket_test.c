9d096bec5d3f8dadc6c909039810703a0326cd4b
salvatorepuccio <46781725+salvatorepuccio@users.noreply.github.com>
2022-03-12 17:41:15 +0100

    R

---
+++ Esercizi/ES8/socket_test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <time.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/un.h>
